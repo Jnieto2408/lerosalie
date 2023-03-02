48b9f7383ab3972c280b50e564533224e603aadb
jnieto2408 <jnieto2408@gmail.com>
2023-03-02 17:52:57 -0500

    minor desing in navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,43 @@
+import { Link } from "react-router-dom"
 import "./Header.scss"
 
 
 const Header = () => {
     return (
         <div className="header">
-            Header
+            <nav>
+                <div className="primary-nav">
+                    <div className="primary-nav-container">
+                        <div className="nav-items">
+                            <div className="nav-item toggle">
+                                <span>
+                                    <div className="menu-toggle-btn">
+                                        <span></span>
+                                    </div>
+                                </span>
+                            </div>
+                            <div className="nav-item logo"><span>LeRosalie</span></div>
+                            <div className="nav-item cart">
+                                <span>cart</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="nav-links">
+                        <div className="nav-info">Informacion o promociones</div>
+                        <div className="nav-links-wrapper">
+                            <div className="nav-link">
+                                <Link>Compra Ya<span>icon</span></Link>
+                            </div>
+                            <div className="nav-link">
+                                <Link>Como funciona<span>icon</span></Link>
+                            </div>
+                            <div className="nav-link">
+                                <Link>Nosotros<span>icon</span></Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
         </div>
     )
 }
