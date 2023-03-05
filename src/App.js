95cce3b31a128a033c0bf71e1077d1a8a0d4c10c
jnieto2408 <jnieto2408@gmail.com>
2023-03-05 14:17:05 -0500

    adding ui to offcanva navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import AppRouter from "./router/AppRouter";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function App() {
   return (
     <AppRouter/>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,66 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import logo from "../../img/logo.svg"
+import "./header.css"
+import { BsWhatsapp, BsTiktok, BsInstagram } from "react-icons/bs";
+import { useState, useEffect } from 'react';
 
 function Header() {
+  const [isBurgerMenuVisible, setIsBurgerMenuVisible] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsBurgerMenuVisible(window.innerWidth <= 991);
+    };
+
+    setIsBurgerMenuVisible(window.innerWidth <= 991);
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home"><img src={logo}></img></Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+    <Navbar key="lg" expand="lg" bg="dark" variant="dark" sticky='top'>
+      <Container fluid>
+        <Navbar.Brand href="#home"><img src={logo} alt="LeRosalie Logo"></img></Navbar.Brand>
+        <Navbar.Toggle aria-controls="offcanvasNavbar-expand-lg" />
+          <Navbar.Offcanvas
+            id="offcanvasNavbar-expand-lg"
+            aria-labelledby="offcanvasNavbarLabel-expand-lg"
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg">
+                Menu
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className={`justify-content-end flex-grow-1 pe-3 ${isBurgerMenuVisible ? "offcanvas-navbar" : ""}`}>
+                <Nav.Link href="/#h">Inicio</Nav.Link>
+                <Nav.Link href="/#h">Nuestras Rosas</Nav.Link>
+                <Nav.Link href="/#h">Preguntas Frecuentes</Nav.Link>
+                <Nav.Link href="/#h">Quienes Somos</Nav.Link>
+                <Nav.Link href="/#h">Contacto</Nav.Link>
+              </Nav>
+              {
+                isBurgerMenuVisible && (
+                  <Container className='mt-4 d-flex gap-5 justify-content-center'>
+                    <a href='https://www.tiktok.com/@lerosalie.co/' target={"_blank"} rel={"noopener noreferrer"} style={{ color: "inherit", textDecoration: "none"}}>
+                    <BsTiktok size={30}/>
+                    </a>
+                    <a href='#whatsapp' target={"_blank"} rel={"noopener noreferrer"} style={{ color: "inherit", textDecoration: "none"}}>
+                    <BsWhatsapp size={30}/>
+                    </a>
+                    <a href='https://www.instagram.com/lerosalie.co/' target={"_blank"} rel={"noopener noreferrer"} style={{ color: "inherit", textDecoration: "none"}}>
+                      <BsInstagram size={30}/>
+                    </a>
+                  </Container>
+                )
+              }
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
       </Container>
     </Navbar>
   );
