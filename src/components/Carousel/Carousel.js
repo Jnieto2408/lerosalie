67d22f7a3c41bdf8b6af1e4d35410a5db94789f6
jnieto2408 <jnieto2408@gmail.com>
2023-03-05 23:54:08 -0500

    placeholders on carousel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -6,10 +6,11 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import "swiper/css/effect-fade";
 import "./carousel.css";
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from "swiper";
+import { EffectFade, Autoplay, Pagination, Navigation } from "swiper";
 
 function Carousel() {
   return (
@@ -17,6 +18,7 @@ function Carousel() {
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
+        effect={"fade"}        
         autoplay={{
           delay: 2500,
           disableOnInteraction: false,
@@ -25,14 +27,14 @@ function Carousel() {
           clickable: true,
         }}
         navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
+        modules={[EffectFade, Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide><img src="https://images.unsplash.com/photo-1529522560205-b5aef3f46189?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="LeRosalie Logo" className='slide'></img></SwiperSlide>
+        <SwiperSlide><img src="https://images.unsplash.com/photo-1587052755556-89808205c097?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="LeRosalie Logo" className='slide'></img></SwiperSlide>
+        <SwiperSlide><img src="https://images.unsplash.com/photo-1571506534699-c76894c03ce7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=786&q=80" alt="LeRosalie Logo" className='slide'></img></SwiperSlide>
+        <SwiperSlide><img src="https://images.unsplash.com/photo-1624585131213-a1b2dd18343d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=949&q=80" alt="LeRosalie Logo" className='slide'></img></SwiperSlide>
+        <SwiperSlide><img src="https://images.unsplash.com/photo-1496857239036-1fb137683000?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="LeRosalie Logo" className='slide'></img></SwiperSlide>
       </Swiper>
     </>
   );
