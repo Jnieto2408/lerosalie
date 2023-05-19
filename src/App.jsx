12835106c7338dc7dcc28303851c8061e7901f16
jnieto2408 <jnieto2408@gmail.com>
2023-05-18 21:29:02 -0500

    finishing merge errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css';
-=======
->>>>>>> b9df2687ec419f69b34b65574dd18d0cc77eb714
 import './App.css'
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <Header/>
-=======
-      <h1>Hello World</h1>
->>>>>>> b9df2687ec419f69b34b65574dd18d0cc77eb714
     </>
   )
 }
