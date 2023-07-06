3108a857e4f8a737d2ec8cd47123835be908a380
jnieto2408 <jnieto2408@gmail.com>
2023-07-05 23:15:13 -0500

    fixing context

--- src/App.jsx
+++ src/App.jsx
@@ -9,33 +9,15 @@ import Contacto from './components/Contacto/Contacto';
 import NuestrasRosas from './components/NuestrasRosas/NuestrasRosas';
 import Footer from './components/Footer/Footer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
-import { CartContext } from './context/CartContext';
-import { useState } from 'react';
 import Cart from './components/Cart/Cart';
+import { CartProvider } from './context/CartContext';
 
 
 const App = () => {
 
-  const [cart, setCart] = useState([])
-  console.log(cart)
-  const agregarAlCarrito = (item) => {
-    setCart([...cart, item])
-  }
-  const isInCart = (id) => {
-    return cart.some(item => item.id === id)
-  }
-  const emptyCart = () => {
-    setCart([])
-  }
-  const totalCart = () => {
-    return cart.reduce((acc, item) => acc + item.price * item.cantidad, 0)
-  }
-  const removerItem = (id) => {
-    setCart(cart.filter(item => item.id !==id))
-  }
-  return (
 
-    <CartContext.Provider value={{cart, agregarAlCarrito, isInCart, emptyCart, totalCart, removerItem}}>
+  return (
+    <CartProvider>
       <BrowserRouter>
           <Navigation/>
           <Routes>
@@ -51,8 +33,7 @@ const App = () => {
           </Routes>
           <Footer/>
       </BrowserRouter>
-    </CartContext.Provider>
-
+    </CartProvider>
   )
 }
 
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,5 +1,29 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 
 export const CartContext = createContext()
 
+export const CartProvider = ({children}) => {
+    const [cart, setCart] = useState([])
+    console.log(cart)
+    const agregarAlCarrito = (item) => {
+        setCart([...cart, item])
+    }
+    const isInCart = (id) => {
+        return cart.some(item => item.id === id)
+    }
+    const emptyCart = () => {
+        setCart([])
+    }
+    const totalCart = () => {
+        return cart.reduce((acc, item) => acc + item.price * item.cantidad, 0)
+    }
+    const removerItem = (id) => {
+        setCart(cart.filter(item => item.id !== id))
+    }
+    return(
+        <CartContext.Provider value={{ cart, agregarAlCarrito, isInCart, emptyCart, totalCart, removerItem }}>
+            {children}
+        </CartContext.Provider>
+    )
+}
