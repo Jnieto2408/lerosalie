0407dd96395910af1871c995434587fa164f09be
jnieto2408 <jnieto2408@gmail.com>
2023-07-09 23:55:47 -0500

    improving cart rendering

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,24 +1,37 @@
 import { useContext } from "react"
 import { CartContext } from "../../context/CartContext"
 import{ FaTrashAlt } from "react-icons/fa"
+import { Link } from "react-router-dom"
 
 
 const Cart = () => {
     const {cart, emptyCart, totalCart, removerItem} = useContext(CartContext)
   return (
     <div className="container my-5">
-      <h2>Tu compra</h2>
-      <hr/>
       {
-        cart.map(item => (
-            <div key={item.id}>
-                <h4>{item.name}</h4>
-                <p>Cantidad: {item.cantidad}</p>
-                <p>Precio: ${item.price * item.cantidad}</p>
-                <button className="btn btn-outline-danger" onClick={() => removerItem(item.id)}><FaTrashAlt/></button>
-                <hr/>
-            </div>
-        ))
+        cart.length === 0
+        ? <>
+            <h2>Tu carrito esta vacio</h2>
+            <hr/>
+            <p>Vuelve al inicio</p>
+            <Link to="/" className="btn btn-primary">Volver</Link>
+        </>
+        :
+        <>
+          <h2>Tu compra</h2>
+          <hr/>
+          {
+            cart.map(item => (
+              <div key={item.id}>
+                    <h4>{item.name}</h4>
+                    <p>Cantidad: {item.cantidad}</p>
+                    <p>Precio: ${item.price * item.cantidad}</p>
+                    <button className="btn btn-outline-danger" onClick={() => removerItem(item.id)}><FaTrashAlt/></button>
+                    <hr/>
+                </div>
+            ))
+          }
+        </>
       }
       <h4>Total: ${totalCart()}</h4>
       <button className="btn btn-danger" onClick={emptyCart}>Vaciar Carrito</button>
